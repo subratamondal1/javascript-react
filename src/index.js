3a611309af06c62f62d4c8cecd302ab92594444c
Subrata Mondal <subratasubha2@gmail.com>
2024-02-15 17:21:18 +0530

    Variables in Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+// We can declare variables in javascript using:
+// 1. var: keyword declares a variable globally or locally to the function where it is defined. It can be redeclared and reassigned.
+// 2. let: keyword declares a variable locally to the block where it is defined. It can be reassigned, but not redeclared.
+// 3. const: keyword declares a constant variable locally to the block where it is defined. It cannot be reassigned or redeclared.
+// 4. Automatic without using any keyword (Not Recommended): declares a variable globally without using any keyword. It can be redeclared and reassigned.
+
+var globalVariable = "India"
+console.log( globalVariable )
+
+let favouriteNumber = 3
+console.log( favouriteNumber )
+
+const nonModifiableVariable = 500
+console.log( nonModifiableVariable )
+
+withoutAnyKeword = "Automatic Variable Defining (Not Recommended)"
+console.log(withoutAnyKeword)
