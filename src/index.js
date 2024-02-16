d2e50baedaff178d80dc9397ca7feeb2826d7791
Subrata Mondal <subratasubha2@gmail.com>
2024-02-16 11:46:51 +0530

    Complete javascrip data types, functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 // 3. const: keyword declares a constant variable locally to the block where it is defined. It cannot be reassigned or redeclared.
 // 4. Automatic without using any keyword (Not Recommended): declares a variable globally without using any keyword. It can be redeclared and reassigned.
 
+/*
 var globalVariable = "India"
 console.log( globalVariable )
 
@@ -14,4 +15,52 @@ const nonModifiableVariable = 500
 console.log( nonModifiableVariable )
 
 withoutAnyKeword = "Automatic Variable Defining (Not Recommended)"
-console.log(withoutAnyKeword)
+console.log( withoutAnyKeword )
+*/
+
+// Numbers in Javascript
+/*
+let a = 5.5
+let b = 4.5
+let c = 2
+
+console.log(typeof a)
+console.log(typeof b)
+console.log( typeof c )
+console.log((a+b)/c)
+*/
+
+// Strings in Javascript
+/*
+let greet = "Hello World"
+console.log( greet, typeof ( greet ) )
+
+let myName = "Subrata Mondal"
+console.log("Hello " + myName)
+*/
+
+// Boolean
+/*
+let a = true
+let b = false
+
+console.log( typeof ( a ), typeof ( b ) )
+console.log(true & false)
+console.log( true && false )
+console.log(true || false)
+*/
+
+// Normal Functions
+function sayHi ()
+{
+    console.log("Hello")
+}
+
+sayHi()
+
+function greet ( name )
+{
+    console.log(name)
+}
+
+greet( "Subrata Mondal" )
