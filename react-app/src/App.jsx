636a11ca7192425b829567bb9f99cd687894e6b4
Subrata Mondal <subratasubha2@gmail.com>
2024-02-17 14:25:27 +0530

    Complete JSX Basics

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1 className='text-red-500 text-3xl'>"Subrata Mondal"</h1>
+    <div className="large" id="largeDiv">
+      <label htmlFor="inputId">First Name</label>
+      <input className="p-2 m-2 border-2 border-red-400" type="number" name="firstName" id="inputId" defaultValue={3}/>
+    </div>
   )
 }
 
