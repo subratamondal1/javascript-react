5a1234bba6ccbf43ca5b8fb3c5783678d3e2a8d9
Subrata Mondal <subratasubha2@gmail.com>
2024-02-17 22:32:48 +0530

    React from Scratch

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import { TodoList } from './TodoList'
+function App ()
+{
   return (
-    <div className="large" id="largeDiv">
-      <label htmlFor="inputId">First Name</label>
-      <input className="p-2 m-2 border-2 border-red-400" type="number" name="firstName" id="inputId" defaultValue={3}/>
+    <div>
+      <h1 className="text-3xl font-bold text-red-700">Todo List</h1>
+      <TodoList/>
     </div>
   )
 }
-
 export default App
