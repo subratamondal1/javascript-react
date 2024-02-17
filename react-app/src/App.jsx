53ce30f0fffd271ba219a989476a0fd95052a759
Subrata Mondal <subratasubha2@gmail.com>
2024-02-17 10:55:02 +0530

    Create React App with Vite

---
+++ react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    "Subrata Mondal"
+  )
+}
+
+export default App
