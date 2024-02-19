80cd700cf2ae2c9de524b60fcc0eaf820f82d882
Subrata Mondal <subratasubha2@gmail.com>
2024-02-19 10:49:08 +0530

    Use Arrays in React Hooks - useState

--- react-crash-course/src/components/Counter.jsx
+++ react-crash-course/src/components/Counter.jsx
@@ -2,39 +2,24 @@ import { useState } from "react"
 
 function Counter ( { count } )
 {
-    const [cart, setCart] = useState({"item":"Apple", "quantity":0})
-    function addApple ()
-        // How to use 'object' in the React Hooks
-        // 1. Use a Callback to get the previous value
-        // 2. Spread out all the properties of the previous state
-        // 3. Only change the property you need to change
+    const [arr, setArr] = useState( [] )
+    function addPlus ()
     {
-        setCart( prevCart =>
-        ({
-            ...prevCart,
-            "quantity": prevCart.quantity + 1
-        }))
+        setArr((prevArr) => [...prevArr, "+"])
     }
-
-    function removeApple ()
-    {
-        setCart( prevCart => (
-            {
-                ...prevCart,
-                "quantity": prevCart.quantity - 1
-            }
-        ))
+    function addMinus(){
+        setArr((prevArr)=> [...prevArr, "-"])
     }
 
     return <div className="flex justify-center items-center text-5xl space-x-4">
         <button
             className="p-4 border-2 border-blue-600 rounded-full"
-            onClick={removeApple}
+            onClick={addMinus}
         >-</button>
-        <h1>{ cart.quantity } { cart.item }</h1>
+        <h1>{ arr }</h1>
         <button
             className="p-4 border-2 border-blue-600 rounded-full"
-            onClick={addApple}
+            onClick={addPlus}
         >+</button>
     </div>
 }
